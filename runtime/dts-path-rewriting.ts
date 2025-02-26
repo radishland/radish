bada877d0d275848bc59fad5fb34503a2311f77e
fcrozatier <frederic.crozatier@protonmail.com>
2025-02-26 14:41:40 +0100

    remove path rewriting script

--- runtime/dts-path-rewriting.ts
+++
@@ -1,7 +0,0 @@
-// This script fixes the path rewriting for ./client/index.d.ts with ts 5.7
-
-import { join } from "@std/path";
-
-const sourcePath = join("client", "index.d.ts");
-const content = Deno.readTextFileSync(sourcePath);
-Deno.writeTextFileSync(sourcePath, content.replaceAll(/\.ts/g, ".d.ts"));
