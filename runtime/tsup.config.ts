6611d9eb0cc7ce400612c6b43af084996745dbfe
fcrozatier <frederic.crozatier@protonmail.com>
2025-02-26 14:40:45 +0100

    bundle with tsup

---
+++ runtime/tsup.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from "tsup";
+
+export default defineConfig({
+  entry: ["src/index.ts"],
+  format: "esm",
+  platform: "node",
+  bundle: true,
+  dts: true,
+  treeshake: true,
+  splitting: false,
+  sourcemap: false,
+  minify: false,
+  target: "esnext",
+  outDir: "client",
+  clean: true,
+  tsconfig: "tsconfig.json",
+  env: {
+    NODE_ENV: "production",
+  },
+});
