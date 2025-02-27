89dc2d25a3653f07e16276a691d72889171a369d
fcrozatier <frederic.crozatier@protonmail.com>
2025-02-27 17:13:35 +0100

    update relative paths

--- core/exports/mod.ts
+++ core/exports/mod.ts
@@ -1,4 +1,4 @@
-export { build, mockGlobals } from "../build.ts";
-export { importMap } from "../generate/impormap.ts";
-export { generateManifest, type Manifest } from "../generate/manifest.ts";
-export { start } from "../start.ts";
+export { build, mockGlobals } from "../src/build.ts";
+export { importMap } from "../src/generate/impormap.ts";
+export { generateManifest, type Manifest } from "../src/generate/manifest.ts";
+export { start } from "../src/start.ts";
