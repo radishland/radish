a509fc2ae92b78ff3bce9866a4d52b0f841c91ca
fcrozatier <frederic.crozatier@protonmail.com>
2025-02-28 16:25:42 +0100

    fill template component

--- init/template/base/elements/my-component/my-component.ts
+++ init/template/base/elements/my-component/my-component.ts
@@ -1,6 +1,15 @@
-import { HandlerRegistry } from "radish";
+import { HandlerRegistry, signal } from "$runtime";
 
 export class MyComponent extends HandlerRegistry {
+  state = signal(0);
+
+  increment = () => {
+    this.state.value++;
+  };
+
+  decrement = () => {
+    this.state.value--;
+  };
 }
 
 if (window && !customElements.get("my-component")) {
