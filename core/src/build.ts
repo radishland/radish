729f78bc28bbf183be78bafd3e82bbcf030ff1ff
fcrozatier <frederic.crozatier@protonmail.com>
2025-03-04 13:52:38 +0100

    better HTMLElement mock

--- core/src/build.ts
+++ core/src/build.ts
@@ -163,15 +163,93 @@ const buildRoute = (
 export const mockGlobals = (): void => {
   // @ts-ignore mock HTMLElement methods on the server to be noop
   globalThis.HTMLElement = class HTMLElement {
-    attachInternals() {}
+    // Event Target
+    addEventListener() {}
+    dispatchEvent() {}
+    removeEventListener() {}
+    // Node
+    appendChild() {}
+    cloneNode() {}
+    compareDocumentPosition() {}
+    contains() {}
+    getRootNode() {}
+    hasChildNodes() {}
+    insertBefore() {}
+    isDefaultNamespace() {}
+    isEqualNode() {}
+    isSameNode() {}
+    lookupNamespaceURI() {}
+    lookupPrefix() {}
+    normalize() {}
+    removeChild() {}
+    replaceChild() {}
+    // Element
+    after() {}
+    animate() {}
+    append() {}
+    attachShadow() {}
+    before() {}
+    checkVisibility() {}
+    closest() {}
+    computedStyleMap() {}
+    getAnimations() {}
+    getAttribute() {}
+    getAttributeNames() {}
+    getAttributeNode() {}
+    getAttributeNodeNS() {}
+    getAttributeNS() {}
+    getBoundingClientRect() {}
+    getClientRects() {}
+    getElementsByClassName() {}
+    getElementsByTagName() {}
+    getElementsByTagNameNS() {}
+    getHTML() {}
+    hasAttribute() {}
+    hasAttributeNS() {}
+    hasAttributes() {}
+    hasPointerCapture() {}
+    insertAdjacentElement() {}
+    insertAdjacentHTML() {}
+    insertAdjacentText() {}
+    matches() {}
+    prepend() {}
     querySelector() {}
+    querySelectorAll() {}
+    releasePointerCapture() {}
+    remove() {}
+    removeAttribute() {}
+    removeAttributeNode() {}
+    removeAttributeNS() {}
+    replaceChildren() {}
+    replaceWith() {}
+    requestFullscreen() {}
+    requestPointerLock() {}
+    scroll() {}
+    scrollBy() {}
+    scrollIntoView() {}
+    scrollTo() {}
+    setAttribute() {}
+    setAttributeNode() {}
+    setAttributeNodeNS() {}
+    setAttributeNS() {}
+    setHTMLUnsafe() {}
+    setPointerCapture() {}
+    toggleAttribute() {}
+    // HTML Element
+    attachInternals() {}
+    blur() {}
+    click() {}
+    focus() {}
+    hidePopover() {}
+    showPopover() {}
+    togglePopover() {}
   };
-  // @ts-ignore no window on the server
-  globalThis.window = undefined;
-  // @ts-ignore no document on the server
+  // @ts-ignore no op document methods on the server
   globalThis.document = {
     querySelector() {},
   };
+  // @ts-ignore no window on the server
+  globalThis.window = undefined;
   // @ts-ignore no customElements on the server
   globalThis.customElements = undefined;
 };
