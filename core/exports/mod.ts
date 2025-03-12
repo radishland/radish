52e3b185cd9b4d744222367b937f081c80b47c4d
fcrozatier <frederic.crozatier@protonmail.com>
2025-03-12 17:53:18 +0100

    adjust exports

--- core/exports/mod.ts
+++ core/exports/mod.ts
@@ -1,4 +1,5 @@
-export { build, mockGlobals } from "../src/generate/build.ts";
+export { setGlobals } from "../src/constants.ts";
+export { build } from "../src/generate/build.ts";
 export { generateImportMap } from "../src/generate/impormap.ts";
 export { generateManifest, type Manifest } from "../src/generate/manifest.ts";
 export { start } from "../src/start.ts";
