f58e58748d8d95a0b9f99db91ccf91082508c35b
fcrozatier <frederic.crozatier@protonmail.com>
2025-03-12 18:09:33 +0100

    expose the Plugin type

---
+++ core/exports/types.ts
@@ -0,0 +1,1 @@
+export type { RadishPlugin } from "../src/types.d.ts";
--- core/src/types.d.ts
+++ core/src/types.d.ts
@@ -1,6 +1,65 @@
+import type { Manifest } from "./generate/manifest.ts";
+import type { SpeculationRules } from "./generate/speculationrules.ts";
+
 export type Maybe<T> = T | undefined;
 export type MaybePromise<T> = T | Promise<T>;
 
-export type Transform = (
-  options: { path: string; content: string },
-) => MaybePromise<string>;
+type TransformResult = string | null | Partial<SourceDescription>;
+
+interface SourceDescription {
+  code: string;
+  ast?: any;
+  meta?: { [plugin: string]: any } | null;
+}
+
+export interface TransformContext {
+  /**
+   * The format of the file as returned by `extname`
+   */
+  format: string;
+  manifest: Manifest;
+  /**
+   * The AST returned by a previous transform plugin
+   */
+  ast?: any;
+  /**
+   * Contains the custom meta-data annotations set by previous transform plugins on this file
+   */
+  meta?: { [plugin: string]: any } | null;
+}
+
+export type BuildOptions = {
+  /**
+   * The speculation rules of the whole site
+   *
+   * https://github.com/WICG/nav-speculation/blob/main/triggers.md
+   *
+   * Spec: https://wicg.github.io/nav-speculation/speculation-rules.html
+   */
+  speculationRules?: SpeculationRules;
+};
+
+export interface RadishPlugin {
+  /**
+   * The name of the plugin with a `radish-plugin-` prefix
+   */
+  name: string;
+  /**
+   * This hook is called at the beginning of every build, and lets you read the options object
+   */
+  buildStart?: (options: BuildOptions) => void;
+  /**
+   * This hook allows to transform individual files
+   *
+   * If you return an `ast` object or `meta` properties, they will be passed to subsequent transforms via the `TransformContext`. This can avoid the need to re-parse files
+   */
+  transform?: (
+    code: string,
+    path: string,
+    context: TransformContext,
+  ) => TransformResult;
+  /**
+   * This hook allows to modify the path where the file will be emitted
+   */
+  emit?: (path: string) => string | null;
+}
