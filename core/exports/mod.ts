d182078e3dcb24b215524aa313f29f034e7218a8
fcrozatier <frederic.crozatier@protonmail.com>
2025-03-19 08:36:05 +0100

    update exports

--- core/exports/mod.ts
+++ core/exports/mod.ts
@@ -1,5 +1,5 @@
 export { globals } from "../src/constants.ts";
 export { build } from "../src/generate/build.ts";
 export { generateImportMap } from "../src/generate/impormap.ts";
-export { generateManifest, type Manifest } from "../src/generate/manifest.ts";
+export { type Manifest } from "../src/generate/manifest.ts";
 export { startApp } from "../src/start.ts";
