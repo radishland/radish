eb41d162841424468f2833526526a6b30f11eb32
fcrozatier <frederic.crozatier@protonmail.com>
2025-03-19 15:51:11 +0100

    add speculation rules

--- init/template/base/start.ts
+++ init/template/base/start.ts
@@ -3,6 +3,25 @@ import type { Config } from "$core/types";
 
 const config: Config = {
   router: { matchers: { number: /\d+/ }, nodeModulesRoot: ".." },
+  // speculationRules: {
+  //   prerender: [{
+  //     where: {
+  //       and: [
+  //         { href_matches: "/*" },
+  //         { not: { href_matches: "/logout" } },
+  //         { not: { href_matches: "/add-to-cart" } },
+  //         { not: { selector_matches: ".do-not-prerender" } },
+  //       ],
+  //     },
+  //     eagerness: "moderate",
+  //   }],
+  //   prefetch: [
+  //     {
+  //       where: { not: { href_matches: "/*" } },
+  //       eagerness: "moderate",
+  //     },
+  //   ],
+  // },
 };
 
 startApp(config);
