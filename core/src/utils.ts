da31b736f1facd3a6daeb4206fae76e409aaef37
Frédéric Crozatier <48696601+fcrozatier@users.noreply.github.com>
2025-03-25 16:12:28 +0100

    refactor(core): use @std toPascalCase utility (#29)

--- core/src/utils.ts
+++ core/src/utils.ts
@@ -1,13 +1,5 @@
 import { basename } from "@std/path/basename";
 
-const capitalize = (input: string) => {
-  return input?.[0]?.toUpperCase() + input.slice(1);
-};
-
-export const kebabToPascal = (input: string): string => {
-  return input.split("-").map(capitalize).join("");
-};
-
 /**
  * Generic memoize decorator for a function with no arguments
  */
