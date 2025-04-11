0f04ef312d117f9c480629aa3e8fc4a3c6f9af74
fcrozatier <frederic.crozatier@protonmail.com>
2025-04-11 10:55:47 +0200

    add filter and fold

--- core/src/monads.ts
+++ core/src/monads.ts
@@ -3,21 +3,31 @@ abstract class Option<T> {
   abstract flatMap<U>(fn: (value: T) => Option<U>): Option<U>;
 
   abstract isSome(): this is Some<T>;
-  abstract isNone(): this is None;
+  abstract isNone(): this is None<T>;
 
   abstract getOrElse(defaultValue: T): T;
+
+  abstract filter(predicate: (option: Option<T>) => boolean): Option<T>;
+  abstract fold<R>(
+    onNone: () => R,
+    onSome: (value: T) => R,
+  ): R;
 
   static some<T>(value: T) {
     return new Some(value);
   }
 
   static none() {
-    return None.instance;
+    return new None();
   }
 }
 
 class Some<T> extends Option<T> {
   #value: T;
+
+  get value() {
+    return this.#value;
+  }
 
   constructor(value: T) {
     super();
@@ -36,39 +46,65 @@ class Some<T> extends Option<T> {
     return true;
   }
 
-  override isNone(): this is None {
+  override isNone(): this is None<T> {
     return false;
   }
 
   override getOrElse(_defaultValue: T): T {
     return this.#value;
   }
+
+  override filter(predicate: (option: Option<T>) => boolean): Option<T> {
+    return predicate(this) ? this : new None();
+  }
+
+  override fold<R>(
+    _onNone: () => R,
+    onSome: (value: T) => R,
+  ): R {
+    return onSome(this.#value);
+  }
+
+  override toString(): string {
+    return `Some(${this.#value})`;
+  }
 }
 
-class None extends Option<never> {
-  static instance = new None();
-
-  private constructor() {
-    super();
+class None<T> extends Option<T> {
+  override map<U>(_fn: (value: T) => U): Option<U> {
+    return new None();
   }
 
-  override map(): None {
-    return this;
+  override flatMap<U>(_fn: (value: T) => Option<U>): Option<U> {
+    return new None();
   }
 
-  override flatMap(): None {
-    return this;
-  }
-
-  override isSome(): this is Some<never> {
+  override isSome(): this is Some<T> {
     return false;
   }
 
-  override isNone(): this is None {
+  override isNone(): this is None<T> {
     return true;
   }
 
   override getOrElse<U>(defaultValue: U): U {
     return defaultValue;
+  }
+
+  override filter(
+    _predicate: (option: Option<T>) => boolean,
+  ): Option<T> {
+    return this;
+  }
+
+  override fold<R>(
+    onNone: () => R,
+    _onSome: (value: T) => R,
+  ): R {
+    return onNone();
+  }
+
+  override toString(): string {
+    return "None";
   }
 }
