94f3340f14023c6e268a513e412dbe52fb2e1a0d
fcrozatier <frederic.crozatier@protonmail.com>
2025-04-16 18:33:02 +0200

    more generic hot update with ttl cache

--- core/exports/plugins.ts
+++ core/exports/plugins.ts
@@ -1,2 +1,2 @@
 export { pluginStripTypes } from "../src/plugins/strip-types.ts";
-export { pluginRadish } from "../src/plugins.ts";
+export { pluginRadish } from "../src/plugins/plugins.ts";
---
+++ core/src/effects/hot_update.ts
@@ -0,0 +1,93 @@
+import { extname, relative } from "@std/path";
+import {
+  elementsFolder,
+  libFolder,
+  routesFolder,
+  staticFolder,
+} from "../constants.ts";
+import { build } from "./build.ts";
+import { generateImportmap } from "./impormap.ts";
+import { manifest } from "./manifest.ts";
+import type { HmrEvent } from "../types.d.ts";
+import { TtlCache } from "../utils/ttl_cache.ts";
+import { createTransformEffect } from "./effects.ts";
+
+type HotUpdateParam = {
+  event: HmrEvent;
+  /**
+   * The list of paths (files, folders, globs) affected by the event which need to
+   * be re-built
+   */
+  paths: string[];
+};
+
+interface Hot {
+  update: (param: HotUpdateParam) => HotUpdateParam;
+}
+
+export const hot = {
+  /**
+   * Triggers the hot update pipeline
+   */
+  update: createTransformEffect<Hot["update"]>("hot/update"),
+};
+
+const hmrEvensCache = new TtlCache<string, HmrEvent>();
+let watcher: Deno.FsWatcher | undefined;
+
+export const startHMR = async (): Promise<void> => {
+  watcher = Deno.watchFs([
+    elementsFolder,
+    routesFolder,
+    libFolder,
+    staticFolder,
+  ], { recursive: true });
+
+  console.log("HRM server watching...");
+
+  for await (const event of watcher) {
+    console.log(`FS Event`, event.kind, event.paths, Date.now());
+
+    for (const path of event.paths) {
+      const relativePath = relative(Deno.cwd(), path);
+      const key = `${event.kind}:${path}`;
+
+      if (!hmrEvensCache.has(key)) {
+        const hmrEvent: HmrEvent = {
+          isFile: !!extname(path),
+          path: relativePath,
+          kind: event.kind,
+          timestamp: Date.now(),
+        };
+        hmrEvensCache.set(key, hmrEvent);
+        await hotUpdate(hmrEvent);
+      }
+
+      // TODO: update router
+      // if (!throttle.has("route")) {
+      //   // Update routes
+      //   if (
+      //     event.paths.some((p) => common([routesPath, p]) === routesPath)
+      //   ) {
+      //     router.generateFileBasedRoutes();
+      //   }
+      //   throttle.add("route");
+      // }
+    }
+  }
+};
+
+const hotUpdate = async (event: HmrEvent) => {
+  const { paths } = await hot.update({
+    event,
+    paths: [event.path],
+  });
+
+  await manifest.write();
+  await manifest.load();
+  await generateImportmap();
+  await build(paths, { incremental: true });
+
+  console.log("Hot-Reloading...");
+  // this.#app.ws.send("reload");
+};
