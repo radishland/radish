82ad3d21c2c0cf2bd8f7869c1fba29bbcc348bf6
fcrozatier <frederic.crozatier@protonmail.com>
2025-04-17 10:47:22 +0200

    (move) AppError to http module

--- core/src/utils/http.ts
+++ core/src/utils/http.ts
@@ -1,4 +1,19 @@
-import { STATUS_TEXT, type StatusCode } from "@std/http";
+import { type ErrorStatus, STATUS_TEXT, type StatusCode } from "@std/http";
+
+export class AppError extends Error {
+  public statusCode: ErrorStatus;
+
+  constructor(
+    statusCode: ErrorStatus,
+    ...params: ConstructorParameters<typeof Error>
+  ) {
+    super(...params);
+    this.statusCode = statusCode;
+    this.name = this.constructor.name;
+
+    Error.captureStackTrace(this, AppError);
+  }
+}
 
 export const createStandardResponse = (
   status: StatusCode,
