96c9fe1974a9190927953bb9b77722bf3dcc9aca
fcrozatier <frederic.crozatier@protonmail.com>
2025-04-23 12:13:25 +0200

    use bdd style

--- core/tests/importmap.test.ts
+++ core/tests/importmap.test.ts
@@ -1,11 +1,12 @@
 import { assertEquals } from "@std/assert/equals";
+import { describe, test } from "@std/testing/bdd";
 import {
   findLongestMatchingPrefix,
   pureImportMap,
 } from "../src/effects/importmap.ts";
 import { assertObjectMatch } from "@std/assert";
 
-Deno.test("matching prefix", () => {
+Deno.test("findLongestMatchingPrefix", () => {
   const cases: {
     args: [specifier: string, prefixes: string[]];
     result: ReturnType<typeof findLongestMatchingPrefix>;
@@ -28,87 +29,89 @@ Deno.test("matching prefix", () => {
   }
 });
 
-Deno.test("main import", () => {
-  const importmap = pureImportMap(
-    { imports: { "a": ["kleur"] } },
-    { "kleur": "npm:kleur@4.1.5" },
-  );
+describe("Importmap", () => {
+  test("main import", () => {
+    const importmap = pureImportMap(
+      { imports: { "a": ["kleur"] } },
+      { "kleur": "npm:kleur@4.1.5" },
+    );
 
-  assertEquals(importmap.imports, {
-    kleur: "https://esm.sh/kleur@4.1.5/",
-  });
-});
-
-Deno.test("subpath import", () => {
-  const importmap = pureImportMap({
-    imports: { a: ["kleur/colors"] },
-  }, {
-    "kleur": "npm:kleur@4.1.5",
+    assertEquals(importmap.imports, {
+      kleur: "https://esm.sh/kleur@4.1.5/",
+    });
   });
 
-  assertEquals(importmap.imports, {
-    "kleur/colors": "https://esm.sh/kleur@4.1.5/colors",
-  });
-});
+  test("subpath import", () => {
+    const importmap = pureImportMap({
+      imports: { a: ["kleur/colors"] },
+    }, {
+      "kleur": "npm:kleur@4.1.5",
+    });
 
-Deno.test("main & subpath imports", () => {
-  const importmap = pureImportMap({
-    imports: { a: ["kleur"], b: ["kleur/colors"] },
-  }, {
-    "kleur": "npm:kleur@4.1.5",
-  });
-
-  assertEquals(importmap.imports, {
-    "kleur/colors": "https://esm.sh/kleur@4.1.5/colors",
-    kleur: "https://esm.sh/kleur@4.1.5/",
-  });
-});
-
-Deno.test("filters unused packages", () => {
-  const importmap = pureImportMap({
-    imports: { a: ["kleur"] },
-  }, {
-    "kleur": "npm:kleur@4.1.5",
-    "shell-quote": "npm:shell-quote@1.8.0",
+    assertEquals(importmap.imports, {
+      "kleur/colors": "https://esm.sh/kleur@4.1.5/colors",
+    });
   });
 
-  assertEquals(importmap.imports, {
-    kleur: "https://esm.sh/kleur@4.1.5/",
-  });
-});
+  test("main & subpath imports", () => {
+    const importmap = pureImportMap({
+      imports: { a: ["kleur"], b: ["kleur/colors"] },
+    }, {
+      "kleur": "npm:kleur@4.1.5",
+    });
 
-Deno.test("aliased relative import", () => {
-  const importmap = pureImportMap({
-    imports: { a: ["$alias"] },
-  }, {
-    "$alias": "./my-module.js",
-  });
-
-  assertEquals(importmap.imports, {
-    "$alias": "/my-module.js",
-  });
-});
-
-Deno.test("rewrite aliased relative ts import", () => {
-  const importmap = pureImportMap({
-    imports: { a: ["$alias"] },
-  }, {
-    "$alias": "./lib/my-module.ts",
+    assertEquals(importmap.imports, {
+      "kleur/colors": "https://esm.sh/kleur@4.1.5/colors",
+      kleur: "https://esm.sh/kleur@4.1.5/",
+    });
   });
 
-  assertEquals(importmap.imports, {
-    "$alias": "/lib/my-module.js",
-  });
-});
+  test("filters unused packages", () => {
+    const importmap = pureImportMap({
+      imports: { a: ["kleur"] },
+    }, {
+      "kleur": "npm:kleur@4.1.5",
+      "shell-quote": "npm:shell-quote@1.8.0",
+    });
 
-Deno.test("https target", () => {
-  const importmap = pureImportMap({
-    imports: { a: ["$alias"] },
-  }, {
-    "$alias": "https://raw.githubusercontent.com/package/module.js",
+    assertEquals(importmap.imports, {
+      kleur: "https://esm.sh/kleur@4.1.5/",
+    });
   });
 
-  assertEquals(importmap.imports, {
-    "$alias": "https://raw.githubusercontent.com/package/module.js",
+  test("aliased relative import", () => {
+    const importmap = pureImportMap({
+      imports: { a: ["$alias"] },
+    }, {
+      "$alias": "./my-module.js",
+    });
+
+    assertEquals(importmap.imports, {
+      "$alias": "/my-module.js",
+    });
+  });
+
+  test("rewrite aliased relative ts import", () => {
+    const importmap = pureImportMap({
+      imports: { a: ["$alias"] },
+    }, {
+      "$alias": "./lib/my-module.ts",
+    });
+
+    assertEquals(importmap.imports, {
+      "$alias": "/lib/my-module.js",
+    });
+  });
+
+  test("https target", () => {
+    const importmap = pureImportMap({
+      imports: { a: ["$alias"] },
+    }, {
+      "$alias": "https://raw.githubusercontent.com/package/module.js",
+    });
+
+    assertEquals(importmap.imports, {
+      "$alias": "https://raw.githubusercontent.com/package/module.js",
+    });
   });
 });
