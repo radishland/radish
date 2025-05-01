aa7474c235b9471d5ac239296cf1b0129f98f0d6
fcrozatier <frederic.crozatier@protonmail.com>
2025-05-01 18:16:28 +0200

    extract file transform logic

--- core/src/plugins/render/mod.ts
+++ core/src/plugins/render/mod.ts
@@ -4,9 +4,8 @@ import { elementsFolder, routesFolder } from "../../constants.ts";
 import { handlerFor } from "../../effects/effects.ts";
 import { Handler } from "../../effects/handlers.ts";
 import { hot } from "../../effects/hot-update.ts";
-import { io } from "../../effects/io.ts";
 import { manifest } from "../../effects/manifest.ts";
-import { type Manifest, render } from "../../effects/render.ts";
+import type { Manifest } from "../../effects/render.ts";
 import type { Plugin } from "../../types.d.ts";
 import { filename, isParent } from "../../utils/path.ts";
 import { updateManifest } from "../manifest.ts";
@@ -14,47 +13,20 @@ import { handleDirectives } from "./directives/mod.ts";
 import { handleManifest, manifestShape } from "./manifest.ts";
 import { handleComponentsAndRoutes } from "./routes_and_components/mod.ts";
 import { handleSort } from "./sort.ts";
-import { assertEmptyHandlerRegistryStack, handleRenderState } from "./state.ts";
-import { handleTransformNode } from "./transform/mod.ts";
+import { handleRenderState } from "./state.ts";
+import { handleTransformNode } from "./transform_node/mod.ts";
+import { handleTransformFile } from "./transformFile.ts";
 
 export const pluginRender: Plugin = {
   name: "plugin-render",
   handlers: [
     handleSort,
+    handleTransformFile,
     ...handleTransformNode,
     ...handleDirectives,
     ...handleRenderState,
     ...handleManifest,
     ...handleComponentsAndRoutes,
-    handlerFor(io.transformFile, async ({ path, content }) => {
-      if (extname(path) !== ".html") return Handler.continue({ path, content });
-
-      const manifestObject = await manifest.get() as Manifest;
-      assertObjectMatch(manifestObject, manifestShape);
-
-      assertEmptyHandlerRegistryStack();
-
-      if (isParent(elementsFolder, path)) {
-        const tagName = filename(path);
-        const element = manifestObject.elements[tagName];
-
-        if (!element) return Handler.continue({ path, content });
-
-        const rendered = await render.component(element);
-        return Handler.continue({ path, content: rendered || content });
-      }
-
-      if (isParent(routesFolder, path)) {
-        const route = manifestObject.routes[path];
-
-        if (!route) return Handler.continue({ path, content });
-
-        const rendered = await render.route(route, "", "");
-        return Handler.continue({ path, content: rendered });
-      }
-
-      return Handler.continue({ path, content });
-    }),
     handlerFor(hot.update, async ({ event, paths }) => {
       const extension = extname(event.path);
       const _manifest = await manifest.get() as Manifest;
---
+++ core/src/plugins/render/transformFile.ts
@@ -0,0 +1,44 @@
+import { extname } from "@std/path";
+import { handlerFor } from "../../effects/effects.ts";
+import { Handler } from "../../effects/handlers.ts";
+import { io } from "../../effects/io.ts";
+import { type Manifest, render } from "../../effects/render.ts";
+import { manifest } from "../../effects/manifest.ts";
+import { manifestShape } from "./manifest.ts";
+import { assertObjectMatch } from "@std/assert";
+import { assertEmptyHandlerRegistryStack } from "./state.ts";
+import { filename, isParent } from "../../utils/path.ts";
+import { elementsFolder, routesFolder } from "../../constants.ts";
+
+export const handleTransformFile = handlerFor(
+  io.transformFile,
+  async ({ path, content }) => {
+    if (extname(path) !== ".html") return Handler.continue({ path, content });
+
+    const manifestObject = await manifest.get() as Manifest;
+    assertObjectMatch(manifestObject, manifestShape);
+
+    assertEmptyHandlerRegistryStack();
+
+    if (isParent(elementsFolder, path)) {
+      const tagName = filename(path);
+      const element = manifestObject.elements[tagName];
+
+      if (!element) return Handler.continue({ path, content });
+
+      const rendered = await render.component(element);
+      return Handler.continue({ path, content: rendered || content });
+    }
+
+    if (isParent(routesFolder, path)) {
+      const route = manifestObject.routes[path];
+
+      if (!route) return Handler.continue({ path, content });
+
+      const rendered = await render.route(route, "", "");
+      return Handler.continue({ path, content: rendered });
+    }
+
+    return Handler.continue({ path, content });
+  },
+);
