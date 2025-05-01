88bb9297471bea0321da57193fa1775c1974b487
fcrozatier <frederic.crozatier@protonmail.com>
2025-05-01 18:07:36 +0200

    add a base transformNode handler

--- core/src/plugins/render/transform/mod.ts
+++ core/src/plugins/render/transform/mod.ts
@@ -1,7 +1,13 @@
+import { handlerFor } from "../../../effects/effects.ts";
+import { render } from "../../../effects/render.ts";
+import { id } from "../../../utils/algebraic-structures.ts";
 import { applyDirectives } from "./apply_directives.ts";
 import { insertTemplates } from "./insert_templates.ts";
+
+const baseHandler = handlerFor(render.transformNode, id);
 
 export const handleTransforms = [
   applyDirectives,
   insertTemplates,
+  baseHandler,
 ];
