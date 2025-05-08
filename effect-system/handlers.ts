b942eb3445cd0222e8284207f1c07cfd1b4b7acb
fcrozatier <frederic.crozatier@protonmail.com>
2025-05-08 19:07:33 +0200

    parametrize handlers by id

--- effect-system/handlers.ts
+++ effect-system/handlers.ts
@@ -1,3 +1,5 @@
+import { assert } from "@std/assert";
+
 /**
  * @internal
  */
@@ -17,17 +19,27 @@ export type BaseHandler<P extends any[], R> = (...payload: P) => MaybePromise<
 type HandlerResult<P extends any[], R> = ReturnType<BaseHandler<P, R>>;
 
 /**
+ * An array of handlers
+ */
+export type Handlers = Handler<any, any>[];
+
+/**
  * This class implements the monadic sequencing of handlers with {@linkcode flatMap} and {@linkcode fold}
  *
  * Developer-facing code will primarily interact with this class through the {@linkcode continue} method
  */
 export class Handler<P extends any[], R> {
+  /**
+   * The effect id this handler is for
+   */
+  id: string;
   #handle: BaseHandler<P, R>;
 
   /**
    * Lifts a {@linkcode BaseHandler} into the monadic `Handler` class
    */
-  constructor(handle: BaseHandler<P, R>) {
+  constructor(id: string, handle: BaseHandler<P, R>) {
+    this.id = id;
     this.#handle = handle;
   }
 
@@ -43,7 +55,7 @@ export class Handler<P extends any[], R> {
    * The monadic binding of handlers, where {@linkcode BaseHandler BaseHandlers} actually are Keisli arrows
    */
   flatMap(f: BaseHandler<P, R>): Handler<P, R> {
-    return new Handler(async (...payload: P) => {
+    return new Handler(this.id, async (...payload: P) => {
       const result = await this.run(...payload);
 
       if (!(result instanceof Continue)) return result;
@@ -53,8 +65,13 @@ export class Handler<P extends any[], R> {
 
   /**
    * Turns a list of handlers into a single handler by sequencing them with {@linkcode flatMap}
+   *
+   * @throws {AssertionError} Throws if all handlers are not handling the same effect
    */
   static fold<P extends any[], R>(handlers: Handler<P, R>[]): Handler<P, R> {
+    const ids = new Set(handlers.map((h) => h.id));
+    assert(ids.size === 1, "Can't fold together handlers of different effects");
+
     return handlers.reduce((acc, curr) =>
       acc.flatMap((...args) => curr.run(...args))
     );
