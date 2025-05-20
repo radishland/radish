feb5b7d4a6d4e1821d0e5af34192bb8e340c741d
fcrozatier <frederic.crozatier@protonmail.com>
2025-05-20 12:04:40 +0200

    export onDispose hook

--- core/exports/mod.ts
+++
@@ -1,4 +0,0 @@
-export { importmapPath } from "$effects/importmap.ts";
-export { manifestPath } from "$effects/manifest.ts";
-export { globals } from "../src/constants.ts";
-export { startApp } from "../src/start.ts";
