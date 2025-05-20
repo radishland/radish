adedacb3bdb63369964e7d55e0086f0cea92fdf8
fcrozatier <frederic.crozatier@protonmail.com>
2025-05-20 17:11:53 +0200

    hmr plugin exports HmrEvent

--- core/src/effects/hmr.ts
+++ core/src/effects/hmr.ts
@@ -1,5 +1,27 @@
 import { createEffect, type EffectWithId } from "@radish/effect-system";
-import type { HmrEvent } from "$lib/types.d.ts";
+
+export type HmrEvent = {
+  /**
+   * Indicates whether the source of the event is a file
+   */
+  isFile: boolean;
+  /**
+   * The path of the entry triggering the event
+   */
+  path: string;
+  /**
+   * The path of the original entry in the case of a rename event
+   */
+  from?: string;
+  /**
+   * The timestamp the event was triggered at
+   */
+  timestamp: number;
+  /**
+   * The kind of the underlying `FsEvent`
+   */
+  kind: Deno.FsEvent["kind"];
+};
 
 type HotUpdateParam = {
   event: HmrEvent;
@@ -10,15 +32,21 @@ type HotUpdateParam = {
   paths: string[];
 };
 
-interface Hot {
+interface Hmr {
+  start: () => void;
   update: (param: HotUpdateParam) => HotUpdateParam;
 }
 
 export const hmr: {
+  start: EffectWithId<[], void>;
   update: EffectWithId<[param: HotUpdateParam], HotUpdateParam>;
 } = {
   /**
+   * Starts the hmr server
+   */
+  start: createEffect<Hmr["start"]>("hmr/start"),
+  /**
    * Triggers the hot update transform
    */
-  update: createEffect<Hot["update"]>("hot/update"),
+  update: createEffect<Hmr["update"]>("hmr/update"),
 };
