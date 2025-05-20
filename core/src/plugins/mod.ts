6b2f0eeb48aba54ab483c9e3f94fbeb75597959d
fcrozatier <frederic.crozatier@protonmail.com>
2025-05-20 14:44:15 +0200

    export plugins from src

---
+++ core/src/plugins/mod.ts
@@ -0,0 +1,9 @@
+export { pluginBuild } from "./build.ts";
+export { pluginConfig } from "./config.ts";
+export { pluginEnv } from "./env/env.ts";
+export { pluginHMR } from "./hmr/hmr.ts";
+export { pluginImportmap } from "./importmap/importmap.ts";
+export { pluginIO } from "./io.ts";
+export { pluginManifest, updateManifest } from "./manifest/manifest.ts";
+export { pluginRender } from "./render/mod.ts";
+export { pluginStripTypes } from "./strip-types.ts";
