aa4c88cf0339344cc22a2accda4c7769acf2bca5
fcrozatier <frederic.crozatier@protonmail.com>
2025-05-22 11:36:28 +0200

    add router tests

--- core/src/plugins/router/router.ts
+++ core/src/plugins/router/router.ts
@@ -2,8 +2,7 @@ import { config } from "$effects/config.ts";
 import { io } from "$effects/io.ts";
 import { manifest } from "$effects/manifest.ts";
 import type { Manifest } from "$effects/render.ts";
-import { type Route, router } from "$effects/router.ts";
-import { onDispose } from "$lib/cleanup.ts";
+import { router } from "$effects/router.ts";
 import { routesFolder } from "$lib/constants.ts";
 import type { Plugin } from "$lib/mod.ts";
 import { manifestShape } from "$lib/plugins/render/hooks/manifest.ts";
@@ -14,18 +13,18 @@ import { serveFile, STATUS_CODE } from "@std/http";
 import { dirname } from "@std/path";
 
 /**
- * The named group can provide an optional regex matcher name
+ * The named group can provide an optional regex matcher
  */
 const square_brackets_around_named_group =
-  /\[([^\]=]+)\]|\[([^\]=]+)=([^\]]+)\]/;
+  /\[(?<namedGroup>[^\]=]+)\]|\[(?<name>[^\]=]+)=(?<matcher>[^\]]+)\]/g;
 
-const routes: Route[] = [];
-
-onDispose(() => {
-  routes.length = 0;
-  console.log("Routes cleared");
-});
-
+/**
+ * Convenient way to create a new route.
+ *
+ * Dynamically creates a {@linkcode Handler} for `router/handle-route`. If both the route method and pattern match on a given request, the provided callback runs.
+ *
+ * This new handler is then dynamically registered
+ */
 const handleAddRoute = handlerFor(router.addRoute, (route) => {
   const newHandler = handlerFor(router.handleRoute, (context) => {
     const patternResult = route.pattern.exec(context.request.url);
@@ -43,19 +42,29 @@ const handleAddRoute = handlerFor(router.addRoute, (route) => {
   });
 
   addHandlers([newHandler]);
-
-  if (!routes.includes(route)) {
-    routes.push(route);
-  }
 });
 
+/**
+ * Default handler for `router/handle-route`
+ *
+ * @returns A standard 404 Not Found `text/plain` response
+ */
 const handleRouterDefaultRouteHandler = handlerFor(router.handleRoute, () => {
   return createStandardResponse(STATUS_CODE.NotFound, {
     headers: { "Content-Type": "text/plain" },
   });
 });
 
-const handleRouterInit = handlerFor(router.init, async () => {
+/**
+ * Performs the `router/add-route` effect for each route of the manifest
+ *
+ * @performs
+ * - `config/read`
+ * - `manifest/get`
+ * - `io/emit-to`
+ * - `router/add-route`
+ */
+export const handleRouterInit = handlerFor(router.init, async () => {
   const rootPath = new RegExp(`^${routesFolder}/?`);
   const { router: routerConfig } = await config.read();
 
@@ -65,14 +74,14 @@ const handleRouterInit = handlerFor(router.init, async () => {
   for (const route of Object.values(_manifest.routes)) {
     const pathname = dirname(route.path)
       .replace(rootPath, "/")
-      .replace(
+      .replaceAll(
         square_brackets_around_named_group,
-        (_match, _, namedGroup, matcherName) => {
+        (_match, namedGroup, name, matcherName) => {
           if (matcherName) {
             const matcher = routerConfig?.matchers?.[matcherName];
             assertExists(matcher, `Regex matcher not found: ${matcherName}`);
 
-            return `:${namedGroup}(${matcher.source})`;
+            return `:${name}(${matcher.source})`;
           }
           return `:${namedGroup}`;
         },
@@ -88,8 +97,6 @@ const handleRouterInit = handlerFor(router.init, async () => {
       },
     });
   }
-
-  console.log(`Routes ${routes.map((r) => r.pattern.pathname)}`);
 });
 
 export const pluginRouter: Plugin = {
