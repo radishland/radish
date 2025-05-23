0a8c426bcec6a09ad155379ef8fa4d6fc95073e2
fcrozatier <frederic.crozatier@protonmail.com>
2025-05-23 11:36:12 +0200

    test the template insertion

--- core/src/plugins/render/transforms/insert-template/insert-templates.ts
+++ core/src/plugins/render/transforms/insert-template/insert-templates.ts
@@ -3,14 +3,16 @@ import { isElementNode } from "@radish/htmlcrunch";
 import { Handler, handlerFor } from "@radish/effect-system";
 import { manifest } from "$effects/manifest.ts";
 import { type Manifest, render } from "$effects/render.ts";
-import { manifestShape } from "../hooks/manifest.ts";
-import { transformNode } from "./transform-node.ts";
+import { manifestShape } from "../../hooks/manifest.ts";
+import { transformNode } from "../transform-node.ts";
 
 /**
+ * Transforms an MElement node by inserting its shadowroot template as the first child
+ *
  * @performs
  * - `manifest/get`
  */
-export const handleInsertTemplatesTransform = handlerFor(
+export const handleRenderTransformInsertTemplate = handlerFor(
   render.transformNode,
   async (node) => {
     if (!isElementNode(node)) return Handler.continue(node);
