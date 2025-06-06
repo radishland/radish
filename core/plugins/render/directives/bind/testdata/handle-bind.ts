637f301eb7f6d07994016d100bc0f2c7bc165f4f
fcrozatier <frederic.crozatier@protonmail.com>
2025-06-06 18:08:28 +0200

    fix unchecked bindings

--- core/plugins/render/directives/bind/testdata/handle-bind.ts
+++ core/plugins/render/directives/bind/testdata/handle-bind.ts
@@ -2,7 +2,10 @@ import { signal } from "@preact/signals-core";
 import { HandlerRegistry } from "@radish/runtime";
 
 export class HandleBind extends HandlerRegistry {
-  checked = signal(true);
+  condition1 = signal(true);
+  condition2 = signal(false);
+  condition3 = signal(false);
+
   content = "hi";
 
   load = signal(false);
