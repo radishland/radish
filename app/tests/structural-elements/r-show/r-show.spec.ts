2bad08197e2f86a7c34122aab31f717d8c87fcd3
Frédéric Crozatier <48696601+fcrozatier@users.noreply.github.com>
2025-06-06 15:49:22 +0200

    feat(runtime): add a fallback slot to `r-show` (#122)
    * add else fallback
    
    * rename back prop to `when`

--- app/tests/structural-elements/r-show/r-show.spec.ts
+++ app/tests/structural-elements/r-show/r-show.spec.ts
@@ -2,7 +2,7 @@ import { expect, test } from "playwright/test";
 
 test("r-show toggles its content", async ({ page }) => {
   const res =
-    await (page.goto("http://localhost:1235/structural-elements/show"));
+    await (page.goto("http://localhost:1235/structural-elements/r-show"));
   expect(res?.status()).toBe(200);
 
   const checkbox = page.getByRole("checkbox");
@@ -10,8 +10,10 @@ test("r-show toggles its content", async ({ page }) => {
   // Content is initially visible
   await expect(checkbox).toBeChecked();
   await expect(page.getByTestId("content")).toBeVisible();
+  await expect(page.getByTestId("fallback")).toBeHidden();
 
-  // Clicking the checkbox toggles the showed content
+  // Clicking the checkbox toggles the content
   await checkbox.click();
-  await expect(page.getByTestId("visible")).toBeHidden();
+  await expect(page.getByTestId("content")).toBeHidden();
+  await expect(page.getByTestId("fallback")).toBeVisible();
 });
