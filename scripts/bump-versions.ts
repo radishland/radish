d060565b3c6c61580faf6b021d5cb8f577db191a
Frédéric Crozatier <48696601+fcrozatier@users.noreply.github.com>
2025-06-11 10:42:27 +0200

    release-2025.06.11 (#133)
    * execute bump script
    
    * execute bump script
    
    * bump versions

--- scripts/bump-versions.ts
+++ scripts/bump-versions.ts
@@ -15,9 +15,27 @@ const getVersion = (module: string) => {
   return JSON.parse(content).version;
 };
 
-const getVersions = () => {
-  const packages = ["core", "effect-system", "htmlcrunch", "init", "runtime"];
-  return Object.fromEntries(packages.map((p) => [p, getVersion(p)]));
+type Versions = {
+  [K in "core" | "effect-system" | "htmlcrunch" | "init" | "runtime"]: string;
 };
 
-getVersions();
+const getVersions = (): Versions => {
+  const packages = ["core", "effect-system", "htmlcrunch", "init", "runtime"];
+  return Object.fromEntries(
+    packages.map((p) => [p, getVersion(p)]),
+  ) as Versions;
+};
+
+const oldVersions = getVersions();
+console.log(" oldVersions:", oldVersions);
+
+const command = new Deno.Command("deno", {
+  args: ["run", "-A", "jsr:@deno/bump-workspaces@0.1.22/cli"],
+  stdout: "inherit",
+  stdin: "inherit",
+});
+
+await command.output();
+
+const newVersions = getVersions();
+console.log(" newVersions:", newVersions);
