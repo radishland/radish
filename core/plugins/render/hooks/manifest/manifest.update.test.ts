79519623e2a6e08e32181d425bb53b7747bf0ff3
fcrozatier <frederic.crozatier@protonmail.com>
2025-06-13 17:09:53 +0200

    fix tests

--- core/plugins/render/hooks/manifest/manifest.update.test.ts
+++ core/plugins/render/hooks/manifest/manifest.update.test.ts
@@ -1,15 +1,17 @@
 import { io } from "$effects/io.ts";
 import { manifest } from "$effects/manifest.ts";
+import {
+  handleManifestGet,
+  handleManifestSet,
+  handleManifestUpdateTerminal,
+} from "$lib/plugins/manifest/manifest.ts";
 import { handlerFor, HandlerScope } from "@radish/effect-system";
 import { assertExists, assertObjectMatch } from "@std/assert";
 import type { WalkEntry } from "@std/fs";
 import { basename } from "@std/path";
 import { describe, test } from "@std/testing/bdd";
-import {
-  handleManifestGet,
-  handleManifestUpdateTerminal,
-} from "../../../manifest/manifest.ts";
 import { handleManifestUpdateRenderHook } from "./manifest.update.ts";
+import { manifestShape } from "./mod.ts";
 
 const createWalkEntry = (path: string): WalkEntry => {
   return {
@@ -36,9 +38,12 @@ describe("manifest", () => {
         return content;
       }),
       handleManifestGet,
+      handleManifestSet,
       handleManifestUpdateRenderHook,
       handleManifestUpdateTerminal,
     );
+
+    await manifest.set(manifestShape);
 
     for (const path of Object.keys(files)) {
       await manifest.update(createWalkEntry(path));
