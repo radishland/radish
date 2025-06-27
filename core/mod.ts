5ec772cf748836b0bdd3b869277ee54e18873191
Frédéric Crozatier <48696601+fcrozatier@users.noreply.github.com>
2025-06-27 11:03:34 +0200

    chore(core): reexport plugins (#156)

--- core/mod.ts
+++ core/mod.ts
@@ -1,6 +1,38 @@
+import {
+  pluginBuild,
+  pluginConfig,
+  pluginEnv,
+  pluginFS,
+  pluginHMR,
+  pluginImportmap,
+  pluginManifest,
+  pluginRender,
+  pluginRouter,
+  pluginServer,
+  pluginStripTypes,
+  pluginWS,
+} from "@radish/core/plugins";
+import { pluginStdElements } from "@radish/std-elements";
+
 export { importmapPath } from "$effects/importmap.ts";
 export { manifestPath } from "$effects/manifest.ts";
 export { onDispose } from "./cleanup.ts";
 export * from "./conventions.ts";
 export { startApp } from "./start.ts";
 export type { Config, ManifestBase } from "./types.d.ts";
+
+export const radishPlugins = [
+  pluginStdElements,
+  pluginWS,
+  pluginServer,
+  pluginRouter,
+  pluginImportmap,
+  pluginRender,
+  pluginManifest,
+  pluginHMR,
+  pluginStripTypes,
+  pluginBuild,
+  pluginEnv,
+  pluginConfig,
+  pluginFS,
+];
